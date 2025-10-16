9f5c7b1f033a3d737fb5bb6e9baec4b672c6f877
BeaverNotACat <beavernotacat@gmail.com>
2025-10-16 23:01:47 +1000

    🐛 Use not forbidden MAC adress IDK WHY FEFOO SOSET IS FORBIDDEN((((

--- hosts/ThinkPadX13S/x13s.nix
+++ hosts/ThinkPadX13S/x13s.nix
@@ -6,8 +6,8 @@
       nixos-x13s = {
         kernel = "mainline";
         enable = true;
-        bluetoothMac = "f3:f0:05:05:37:00"; # FEFOO SOSET
-        wifiMac = "f3:f0:05:05:37:00"; # FEFOO SOSET
+        bluetoothMac = "00:03:7f:12:ed:93";
+        wifiMac = "00:03:7f:12:ed:93";
       };
       nixpkgs.config.allowUnfree = true;
     }
