b1cdacaaca20b2981acc7bebb5b3fd5c1f99c9f8
BeaverNotACat <beavernotacat@gmail.com>
2025-10-17 20:24:39 +1000

    🐛 Change blutoth mac to already got

--- hosts/ThinkPadX13S/x13s.nix
+++ hosts/ThinkPadX13S/x13s.nix
@@ -4,7 +4,7 @@
     x13s.nixosModules.default
     {
       nixos-x13s = {
-        kernel = "jhovold";
+        kernel = "mainline";
         enable = true;
         bluetoothMac = "53:28:11:7B:07:A2";
         wifiMac = "00:03:7f:12:ed:93";
